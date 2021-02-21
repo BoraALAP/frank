923ecea4d0eff25fd75720d7b570f444bb6a0b16
BoraALAP <bora.alap@artticfox.com>
2021-02-21 14:26:44 -0500

    fixed front end

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "3a65c795b216ed62f8ba";
+/******/ 	var hotCurrentHash = "df9b289e42878b68748d";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
