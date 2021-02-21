a589c27bc8363702cd0092768809c9f8f8bacd19
BoraALAP <bora.alap@artticfox.com>
2021-02-21 01:53:58 -0500

    push

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "9e26a5706e7b8a4ee1a2";
+/******/ 	var hotCurrentHash = "c8f7a0428ccc292a9d24";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
