bcc041e59aea374fe2f9bf23dde25adb658b01c7
BoraALAP <bora.alap@artticfox.com>
2021-02-27 22:03:13 -0500

    push

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -1,93 +1,39 @@
 import 'dotenv/config';
-import { createAuth } from '@keystone-next/auth';
 import { config, createSchema } from '@keystone-next/keystone/schema';
 import {
   withItemData,
   statelessSessions,
 } from '@keystone-next/keystone/session';
-import { permissionsList } from './schemas/fields';
-import { Role } from './schemas/Role';
-import { Screen } from './schemas/Options/Screen';
-import { Interior } from './schemas/Options/Interior';
-import { HardwareKit } from './schemas/Options/HardwareKit';
-import { GlassColor } from './schemas/Options/GlassColor';
-import { Glass } from './schemas/Options/Glass';
-import { Exterior } from './schemas/Options/Exterior';
-import { DividedLite } from './schemas/Options/DividedLite';
-import { BrickmouldAndTrim } from './schemas/Options/BrickmouldAndTrim';
-
-import { ProductCategory } from './schemas/ProductCategory';
-import { Product } from './schemas/Product';
-
-import { Operation } from './schemas/Operation';
-import { Imagine } from './schemas/Imagine';
-import { Dealer } from './schemas/Dealer';
-import { ContactUsForm } from './schemas/ContactUsForm';
 import { User } from './schemas/User';
 
-import { forgotPasswordEmail } from './lib/mail';
 // import { extendGraphqlSchema } from './mutations';
-
-function check(name: string) {}
 
 const sessionConfig = {
   maxAge: 60 * 60 * 24 * 360, // How long they stay signed in?
   secret: process.env.COOKIE_SECRET,
 };
 
-const { withAuth } = createAuth({
-  listKey: 'User',
-  identityField: 'email',
-  secretField: 'password',
-  initFirstItem: {
-    fields: ['firstName', 'lastName', 'email', 'password', 'companyName'],
+export default config({
+  lists: createSchema({
+    User,
+  }),
+  db: {
+    adapter: 'mongoose',
+    url: process.env.DATABASE_URL,
   },
-  passwordResetLink: {
-    async sendToken(args) {
-      // send the email
-      await forgotPasswordEmail(args.token, args.identity);
+  // extendGraphqlSchema,
+  ui: {
+    // Show the UI only for poeple who pass this test
+    isAccessAllowed: ({ session }) => !!session,
+  },
+  server: {
+    cors: {
+      origin: [process.env.FRONTEND_URL],
+      credentials: true,
     },
   },
+  session: withItemData(statelessSessions(sessionConfig), {
+    // GraphQL Query
+    User: `id name email role`,
+  }),
 });
-
-export default withAuth(
-  config({
-    lists: createSchema({
-      User,
-      Role,
-      Product,
-      ProductCategory,
-      Operation,
-      Imagine,
-      Dealer,
-      ContactUsForm,
-      Exterior,
-      Interior,
-      Glass,
-      GlassColor,
-      HardwareKit,
-      Screen,
-      DividedLite,
-      BrickmouldAndTrim,
-    }),
-    db: {
-      adapter: 'mongoose',
-      url: process.env.DATABASE_URL,
-    },
-    // extendGraphqlSchema,
-    ui: {
-      // Show the UI only for poeple who pass this test
-      isAccessAllowed: ({ session }) => !!session,
-    },
-    server: {
-      cors: {
-        origin: [process.env.FRONTEND_URL],
-        credentials: true,
-      },
-    },
-    session: withItemData(statelessSessions(sessionConfig), {
-      // GraphQL Query
-      User: `id name email role { ${permissionsList.join(' ')} }`,
-    }),
-  })
-);
--- backend/schemas/User.ts
+++ backend/schemas/User.ts
@@ -1,26 +1,11 @@
-import { text, password, relationship } from '@keystone-next/fields';
+import { text, password } from '@keystone-next/fields';
 
 import { list } from '@keystone-next/keystone/schema';
-
-import { permissions, rules } from '../access';
 // import { sendEmail } from "../mail";
 
 export const User = list({
-  access: {
-    create: () => true,
-    read: rules.canManageUsers,
-    update: rules.canManageUsers,
-    // only people with the permission can delete themselves!
-    // You can't delete yourself
-    delete: permissions.canManageRoles,
-  },
-  ui: {
-    // hide the backend UI from regular users
-    hideCreate: (args) => !permissions.canManageUsers(args),
-    hideDelete: (args) => !permissions.canManageUsers(args),
-    isHidden: (args) => !permissions.canManageUsers(args),
-    labelField: 'firstName',
-  },
+  access: true,
+
   fields: {
     firstName: text({ isRequired: true, isIndexed: true }),
     lastName: text({ isRequired: true }),
@@ -28,16 +13,5 @@ export const User = list({
     password: password(),
     companyName: text({ isRequired: true }),
     dealerId: text({}),
-    role: relationship({
-      ref: 'Role.assignedTo',
-      access: {
-        create: permissions.canManageRoles,
-        update: permissions.canManageUsers,
-      },
-      ui: {
-        createView: { fieldMode: 'hidden' },
-        itemView: { fieldMode: 'edit' },
-      },
-    }),
   },
 });
